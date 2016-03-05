71d5112dcf297b565c48ac070e2a148adc500d92
胡哲 <286808713@qq.com>
2016-03-05 21:56:41 +0800

    增加coding中mysql、mongodb、redis、fs 配置

---
+++ codingConfig.js
@@ -0,0 +1,36 @@
+/**
+ * Created by vuji on 16/3/5.
+ */
+
+var codingConfig = {};
+
+codingConfig.mongodb = {
+    host: "10.9.58.169",
+    name: "b4463dbb-7824-40a3-a3ec-fa2b6799e29e",
+    password: "o8CgfwqfEPczcnAoJINCrg",
+    port: "27017",
+    uri: "mongodb://f04b0785-6de7-4280-b298-11bc9b62283c:o8CgfwqfEPczcnAoJINCrg@10.9.58.169:27017/b4463dbb-7824-40a3-a3ec-fa2b6799e29e",
+    username: "f04b0785-6de7-4280-b298-11bc9b62283c"
+}
+
+codingConfig.mysql = {
+    hostname: "10.9.1.188",
+    jdbcUrl: "jdbc:mysql://10.9.1.188:3306/cf_08b786cd_8562_4b99_873a_dae524db097e?user=xSTPWxm9TzPff5io&password=C2aUJ3v5JbIJLYOM",
+    name: "cf_08b786cd_8562_4b99_873a_dae524db097e",
+    password: "C2aUJ3v5JbIJLYOM",
+    port: "3306",
+    uri: "mysql://xSTPWxm9TzPff5io:C2aUJ3v5JbIJLYOM@10.9.1.188:3306/cf_08b786cd_8562_4b99_873a_dae524db097e?reconnect=true",
+    username: "xSTPWxm9TzPff5io"
+}
+
+codingConfig.redis = {
+    host: "10.9.21.212",
+    hostname: "10.9.21.212",
+    name: "19ecc222-ba38-45a2-bca1-fc7f701f60d3",
+    password: "3ab7efdf-d4fa-4ea7-b077-4820618bf045",
+    port: "5109"
+}
+
+codingConfig.fs = {
+        host_path: "/home/vcap/fs/bfd61ff6110272c"
+}
